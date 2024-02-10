a069dc7e0cc6a122c06617412969a6198cca18e7
RishavGaur <rishav021gaur@gmail.com>
2024-02-10 11:06:09 +0530

    3rd Program

---
+++ Conditional/divisiblity.js
@@ -0,0 +1,7 @@
+let num= prompt("Enter a Number: ");
+console.log("You've entered ",num);
+if(num%5==0){
+    console.log("Given Number is Divisible by 5");
+}else{
+    console.log("Number is not divisible by 5");
+}
